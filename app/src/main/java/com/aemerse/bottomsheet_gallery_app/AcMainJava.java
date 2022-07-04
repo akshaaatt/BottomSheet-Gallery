4e772b884aa700f285877593a33512835c17cda7
Akshat Tiwari <tiwariakshat03@gmail.com>
2022-07-04 11:58:28 +0530

    Cleanup example app

--- app/src/main/java/com/aemerse/bottomsheet_gallery_app/AcMainJava.java
+++ app/src/main/java/com/aemerse/bottomsheet_gallery_app/AcMainJava.java
@@ -4,13 +4,12 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.view.LayoutInflater;
-import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.aemerse.bottomsheet_gallery.BottomSheetImagePicker;
+import com.aemerse.bottomsheet_gallery_app.databinding.AcMainBinding;
 import com.bumptech.glide.Glide;
 
 import org.jetbrains.annotations.NotNull;
@@ -20,27 +19,21 @@ import java.util.List;
 
 public class AcMainJava extends AppCompatActivity implements BottomSheetImagePicker.OnImagesSelectedListener {
 
-    private ViewGroup imageContainer;
+    private AcMainBinding binding;
 
     @Override
     public void onCreate(@androidx.annotation.Nullable Bundle savedInstanceState, @androidx.annotation.Nullable PersistableBundle persistentState) {
         super.onCreate(savedInstanceState, persistentState);
+        binding = AcMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
-        setContentView(R.layout.ac_main);
-
-        Button btnSingle = findViewById(R.id.btnPickSingle);
-        Button btnMulti = findViewById(R.id.btnPickMulti);
-        Button btnClear = findViewById(R.id.btnClear);
-
-        imageContainer = findViewById(R.id.imageContainer);
-
-        btnClear.setOnClickListener(v -> imageContainer.removeAllViews());
-        btnSingle.setOnClickListener(v -> new BottomSheetImagePicker.Builder(getString(R.string.file_provider))
+        binding.btnClear.setOnClickListener(v -> binding.imageContainer.removeAllViews());
+        binding.btnPickSingle.setOnClickListener(v -> new BottomSheetImagePicker.Builder(getString(R.string.file_provider))
                 .singleSelectTitle(R.string.pick_single)
                 .peekHeight(R.dimen.peekHeight)
                 .requestTag("single")
                 .show(getSupportFragmentManager(), null));
-        btnMulti.setOnClickListener(v -> new BottomSheetImagePicker.Builder(getString(R.string.file_provider))
+        binding.btnPickMulti.setOnClickListener(v -> new BottomSheetImagePicker.Builder(getString(R.string.file_provider))
                 .columnSize(R.dimen.columnSize)
                 .multiSelect(3, 6)
                 .multiSelectTitles(
@@ -54,10 +47,10 @@ public class AcMainJava extends AppCompatActivity implements BottomSheetImagePic
 
     @Override
     public void onImagesSelected(@NotNull List<? extends Uri> uris, @Nullable String tag) {
-        imageContainer.removeAllViews();
+        binding.imageContainer.removeAllViews();
         for (Uri uri : uris) {
-            ImageView iv = (ImageView) LayoutInflater.from(this).inflate(R.layout.scrollitem_image, imageContainer, false);
-            imageContainer.addView(iv);
+            ImageView iv = (ImageView) LayoutInflater.from(this).inflate(R.layout.scrollitem_image, binding.imageContainer, false);
+            binding.imageContainer.addView(iv);
             Glide.with(this).load(uri).into(iv);
         }
     }
